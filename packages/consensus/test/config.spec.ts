c9d0a9e323f113dd3fbb6008d7903f239f414a8a
George Avsetsin <mail@sjors.ru>
2022-01-25 18:13:07 +0300

    test: consensus config

---
+++ packages/consensus/test/config.spec.ts
@@ -0,0 +1,49 @@
+jest.mock('node-fetch');
+
+import { Test } from '@nestjs/testing';
+import { FetchService } from '@lido-nestjs/fetch';
+import { ConsensusModule, ConsensusService } from '../src';
+
+describe('Config endpoints', () => {
+  let consensusService: ConsensusService;
+  let fetchService: FetchService;
+  let mockFetch: jest.SpyInstance<
+    ReturnType<FetchService['fetchJson']>,
+    Parameters<FetchService['fetchJson']>
+  >;
+
+  beforeEach(async () => {
+    const module = { imports: [ConsensusModule.forFeature()] };
+    const moduleRef = await Test.createTestingModule(module).compile();
+    consensusService = moduleRef.get(ConsensusService);
+    fetchService = moduleRef.get(FetchService);
+
+    mockFetch = jest
+      .spyOn(fetchService, 'fetchJson')
+      .mockImplementation(async () => null);
+  });
+
+  test('getForkSchedule', async () => {
+    await consensusService.getForkSchedule();
+
+    expect(mockFetch).toBeCalledTimes(1);
+    expect(mockFetch).toBeCalledWith('/eth/v1/config/fork_schedule', undefined);
+  });
+
+  test('getSpec', async () => {
+    await consensusService.getSpec();
+
+    expect(mockFetch).toBeCalledTimes(1);
+    expect(mockFetch).toBeCalledWith('/eth/v1/config/spec', undefined);
+  });
+
+  test('getDepositContract', async () => {
+    await consensusService.getDepositContract();
+
+    expect(mockFetch).toBeCalledTimes(1);
+    expect(mockFetch).toBeCalledWith(
+      '/eth/v1/config/deposit_contract',
+      undefined,
+    );
+  });
+});
