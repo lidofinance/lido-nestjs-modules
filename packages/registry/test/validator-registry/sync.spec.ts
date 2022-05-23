8cb2b9f3fb378a3a2cb06cd6c83f6fa5b687fde0
Eddort <webisforce@gmail.com>
2022-05-23 13:51:00 +0400

    feat: validator-registry tests

---
+++ packages/registry/test/validator-registry/sync.spec.ts
@@ -0,0 +1,77 @@
+import { MikroOrmModule } from '@mikro-orm/nestjs';
+import { nullTransport, LoggerModule } from '@lido-nestjs/logger';
+import { ModuleMetadata } from '@nestjs/common';
+import { getNetwork } from '@ethersproject/networks';
+import { getDefaultProvider, Provider } from '@ethersproject/providers';
+import { Test } from '@nestjs/testing';
+import {
+  RegistryModule,
+  RegistryService,
+  RegistryStorageService,
+} from '../../src';
+
+describe('Sync module initializing', () => {
+  const provider = getDefaultProvider('mainnet');
+
+  jest
+    .spyOn(provider, 'detectNetwork')
+    .mockImplementation(async () => getNetwork('mainnet'));
+
+  const testModules = async (metadata: ModuleMetadata) => {
+    const moduleRef = await Test.createTestingModule(metadata).compile();
+    const registryService: RegistryService = moduleRef.get(RegistryService);
+    const storageService = moduleRef.get(RegistryStorageService);
+
+    await storageService.onModuleInit();
+    expect(registryService).toBeDefined();
+    await storageService.onModuleDestroy();
+  };
+
+  test('forRoot', async () => {
+    const imports = [
+      MikroOrmModule.forRoot({
+        dbName: ':memory:',
+        type: 'sqlite',
+        allowGlobalContext: true,
+        entities: ['./packages/registry/**/*.entity.ts'],
+      }),
+      LoggerModule.forRoot({ transports: [nullTransport()] }),
+      RegistryModule.forRoot({ provider }),
+    ];
+    await testModules({ imports });
+  });
+
+  test('forFeature', async () => {
+    const imports = [
+      MikroOrmModule.forRoot({
+        dbName: ':memory:',
+        type: 'sqlite',
+        allowGlobalContext: true,
+        entities: ['./packages/registry/**/*.entity.ts'],
+      }),
+      LoggerModule.forRoot({ transports: [nullTransport()] }),
+      RegistryModule.forFeature({ provider }),
+    ];
+    await testModules({ imports });
+  });
+
+  test('forFeature + global provider', async () => {
+    const imports = [
+      MikroOrmModule.forRoot({
+        dbName: ':memory:',
+        type: 'sqlite',
+        allowGlobalContext: true,
+        entities: ['./packages/registry/**/*.entity.ts'],
+      }),
+      LoggerModule.forRoot({ transports: [nullTransport()] }),
+      RegistryModule.forFeature(),
+    ];
+
+    const metadata = {
+      imports,
+      providers: [{ provide: Provider, useValue: provider }],
+    };
+
+    await testModules(metadata);
+  });
+});
