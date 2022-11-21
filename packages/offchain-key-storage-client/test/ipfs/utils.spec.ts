92d1db4c4e5a487bd2d220497d49c01823193b51
Anna Mukharram <a.mukharram@fun-box.ru>
2022-11-21 18:47:40 +0400

    fix: add test

--- packages/offchain-key-storage-client/test/ipfs/utils.spec.ts
+++ packages/offchain-key-storage-client/test/ipfs/utils.spec.ts
@@ -8,6 +8,7 @@ test('has all listed attributes', () => {
 test('doesnt have all listed attributes', () => {
   const param = { key: 'key1' };
   expect(hasAttributes(param, ['key', 'sign'])).toBe(false);
+  expect(hasAttributes(param, ['key1', 'sign'])).toBe(false);
 
   expect(hasAttributes(undefined, ['key', 'sign'])).toBe(false);
   expect(hasAttributes(null, ['key', 'sign'])).toBe(false);
