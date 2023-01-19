b65e958469c7e1bee861b9665397353fcbb67a9b
infloop <infloop.gm@gmail.com>
2023-01-19 21:46:11 +0300

    fix: codestyle

--- packages/validators-registry/src/storage/consensus-validator.repository.ts
+++ packages/validators-registry/src/storage/consensus-validator.repository.ts
@@ -1,6 +1,4 @@
 import { EntityRepository } from '@mikro-orm/knex';
 import { ConsensusValidatorEntity } from './consensus-validator.entity';
 
-export class ConsensusValidatorRepository extends EntityRepository<ConsensusValidatorEntity> {
-
-}
+export class ConsensusValidatorRepository extends EntityRepository<ConsensusValidatorEntity> {}
