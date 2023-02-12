5bce3456169275dcfc1f3ea2bfe503437f462d9b
infloop <infloop.gm@gmail.com>
2023-02-12 14:05:08 +0300

    feat: add more contracts for withdrawals

---
+++ packages/contracts/src/lido-locator/lido-locator.constants.ts
@@ -0,0 +1,7 @@
+import { CHAINS } from '@lido-nestjs/constants';
+
+export const LIDO_LOCATOR_CONTRACT_TOKEN = Symbol('lidoLocatorContract');
+
+export const LIDO_LOCATOR_CONTRACT_ADDRESSES = {
+  [CHAINS.Zhejiang]: '0x12cd349E19Ab2ADBE478Fc538A66C059Cf40CFeC',
+};
---
+++ packages/contracts/src/staking-router/staking-router.constants.ts
@@ -0,0 +1,7 @@
+import { CHAINS } from '@lido-nestjs/constants';
+
+export const STAKING_ROUTER_CONTRACT_TOKEN = Symbol('stakingRouterContract');
+
+export const STAKING_ROUTER_CONTRACT_ADDRESSES = {
+  [CHAINS.Zhejiang]: '0xDd7d15490748a803AeC6987046311AF76a5A6502',
+};
