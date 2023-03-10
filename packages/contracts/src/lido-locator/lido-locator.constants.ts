710edb505445a4982f8e3b50e76e67fd5e7705a1
infloop <infloop.gm@gmail.com>
2023-03-10 03:41:25 +0300

    feat!: Zhejiang testnet re-deploy
    BREAKING CHANGE: add new versions of contracts

--- packages/contracts/src/lido-locator/lido-locator.constants.ts
+++ packages/contracts/src/lido-locator/lido-locator.constants.ts
@@ -3,5 +3,5 @@ import { CHAINS } from '@lido-nestjs/constants';
 export const LIDO_LOCATOR_CONTRACT_TOKEN = Symbol('lidoLocatorContract');
 
 export const LIDO_LOCATOR_CONTRACT_ADDRESSES = {
-  [CHAINS.Zhejiang]: '0x12cd349E19Ab2ADBE478Fc538A66C059Cf40CFeC',
+  [CHAINS.Zhejiang]: '0x548C1ED5C83Bdf19e567F4cd7Dd9AC4097088589',
 };
