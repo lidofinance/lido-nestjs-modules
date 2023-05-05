ac0a0ab5e41849ad1c4ea005903fc7ed61b92b0e
George Avsetsin <mail@sjors.ru>
2023-05-05 12:06:33 +0400

    feat: lido v2 contracts
    BREAKING CHANGE: abi and addresses

--- packages/contracts/src/staking-router/staking-router.constants.ts
+++ packages/contracts/src/staking-router/staking-router.constants.ts
@@ -3,6 +3,7 @@ import { CHAINS } from '@lido-nestjs/constants';
 export const STAKING_ROUTER_CONTRACT_TOKEN = Symbol('stakingRouterContract');
 
 export const STAKING_ROUTER_CONTRACT_ADDRESSES = {
+  [CHAINS.Mainnet]: '0xFdDf38947aFB03C621C71b06C9C70bce73f12999',
   [CHAINS.Goerli]: '0xa3Dbd317E53D363176359E10948BA0b1c0A4c820',
   [CHAINS.Zhejiang]: '0x0Ed4aCd69f6e00a2Ca0d141f8A900aC6BFaF70F0',
 };
