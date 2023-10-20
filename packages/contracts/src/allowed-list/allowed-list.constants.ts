47ff349e4c87e51f01bce1df03185c51326d61b4
George Avsetsin <mail@sjors.ru>
2023-10-20 11:49:15 +0300

    feat: allowed relay address

--- packages/contracts/src/allowed-list/allowed-list.constants.ts
+++ packages/contracts/src/allowed-list/allowed-list.constants.ts
@@ -2,9 +2,8 @@ import { CHAINS } from '@lido-nestjs/constants';
 
 export const ALLOWED_LIST_CONTRACT_TOKEN = Symbol('allowedListContract');
 
-// TODO: add mainnet and holesky addresses
 export const ALLOWED_LIST_CONTRACT_ADDRESSES = {
-  // [CHAINS.Mainnet]: 'TODO',
+  [CHAINS.Mainnet]: '0xF95f069F9AD107938F6ba802a3da87892298610E',
   [CHAINS.Goerli]: '0xeabe95ac5f3d64ae16acbb668ed0efcd81b721bc',
-  // [CHAINS.Holesky]: 'TODO',
+  [CHAINS.Holesky]: '0x2d86C5855581194a386941806E38cA119E50aEA3',
 };
