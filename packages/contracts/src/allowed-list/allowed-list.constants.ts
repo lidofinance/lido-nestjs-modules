19760974a49a1e640c32405689c142e9e38df866
Alexander Movsunov <alex.mov@lido.fi>
2024-03-12 21:19:40 +0400

    feat(val-715): allowed relay list contracts

--- packages/contracts/src/allowed-list/allowed-list.constants.ts
+++ packages/contracts/src/allowed-list/allowed-list.constants.ts
@@ -2,9 +2,8 @@ import { CHAINS } from '@lido-nestjs/constants';
 
 export const ALLOWED_LIST_CONTRACT_TOKEN = Symbol('allowedListContract');
 
-// TODO: add mainnet and holesky addresses
 export const ALLOWED_LIST_CONTRACT_ADDRESSES = {
-  // [CHAINS.Mainnet]: 'TODO',
+  [CHAINS.Mainnet]: '0xf95f069f9ad107938f6ba802a3da87892298610e',
+  [CHAINS.Holesky]: '0x2d86c5855581194a386941806e38ca119e50aea3',
   [CHAINS.Goerli]: '0xeabe95ac5f3d64ae16acbb668ed0efcd81b721bc',
-  // [CHAINS.Holesky]: 'TODO',
 };
