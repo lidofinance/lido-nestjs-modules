5a54291f3ef31aa3c53e8c1dbdab349b609e6125
Eddort <webisforce@gmail.com>
2025-03-17 16:25:47 +0100

    feat: add Hoodi chain addresses to various contract constants

--- packages/contracts/src/easytrack/easytrack.constants.ts
+++ packages/contracts/src/easytrack/easytrack.constants.ts
@@ -2,9 +2,10 @@ import { CHAINS } from '@lido-nestjs/constants';
 
 export const EASYTRACK_CONTRACT_TOKEN = Symbol('easyTrackContract');
 
-// TODO: add holesky address
+// TODO: add hoodi address
 export const EASYTRACK_CONTRACT_ADDRESSES = {
   [CHAINS.Mainnet]: '0xF0211b7660680B49De1A7E9f25C65660F0a13Fea',
   [CHAINS.Goerli]: '0xAf072C8D368E4DD4A9d4fF6A76693887d6ae92Af',
   [CHAINS.Holesky]: '0x1763b9ED3586B08AE796c7787811a2E1bc16163a',
+  [CHAINS.Hoodi]: '0x0000000000000000000000000000000000000000',
 };
