36ccbdfcd6f87323fd1af544475950cd1ec130be
Dmitrii <lost.ping@gmail.com>
2025-12-10 22:29:54 +0500

    fix(security): update zod to fix DoS vulnerability
    Update zod ^3.20.2 -> ^3.23.0 in @lido-nestjs/validators-registry
    to address CVE-2023-4316 denial of service vulnerability.

--- packages/validators-registry/src/utils/parse.ts
+++ packages/validators-registry/src/utils/parse.ts
@@ -1,16 +1,15 @@
 import { z } from 'zod';
-import { ZodType } from 'zod/lib/types';
 
 /**
  * Parse Zod Type. If parsing fails - onFail in executed.
  *
  * onFail must throw Error
  */
-export const parseAsTypeOrFail = <Output, Def, Input>(
-  type: ZodType<Output, Def, Input>,
+export const parseAsTypeOrFail = <Output, Def extends z.ZodTypeDef, Input>(
+  type: z.ZodType<Output, Def, Input>,
   data: unknown,
   onFail: (error: z.ZodError) => never,
-) => {
+): Output => {
   const res = type.safeParse(data);
   if (res.success) {
     return res.data;
